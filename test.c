0db0f2380f717c44ec70ae53d8461e2ea4634ead
anplixFz <anplixfz@gmail.com>
2023-03-18 16:13:20 +0900

    Initial project version

---
+++ test.c
@@ -0,0 +1,14 @@
+/**
+ * main.c
+ * bong_Test
+ * Created by Vu Ai Linh on 2023/03/18.
+ **/
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, BONGKUTE!\n");
+    return 0;
+}
+
